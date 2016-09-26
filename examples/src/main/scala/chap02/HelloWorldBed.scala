6cd2811c4ae681c345a32dd2034f374841031206
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-09-26 14:55:51 +0200

    [WIP] helloworld with sideofbed - probability is the same, so it doesn't work

--- examples/src/main/scala/chap02/HelloWorldBed.scala
+++ examples/src/main/scala/chap02/HelloWorldBed.scala
@@ -12,24 +12,24 @@ object HelloWorldFigaroBed {
 		if(s == "wrong") Flip(0.0)
 		else Flip(0.2))
 	)*/
-	/*val greetingToday = If(wrongSideOfBed, "Oh no, not again",
+	val greetingToday = If(wrongSideOfBed, "Oh no, not again",
 		If(sunnyToday,
 			Select(0.6 -> "Hello, world!", 0.4 -> "Howdy, universe!"),
 			Select(0.2 -> "Hello, world!", 0.8 -> "Oh no, not again")
 		)
-	)*/
-	val greetingToday = If(sunnyToday,
+	)
+	/*val greetingToday = If(sunnyToday,
 			Select(0.6 -> "Hello, world!", 0.4 -> "Howdy, universe!"),
 			Select(0.2 -> "Hello, world!", 0.8 -> "Oh no, not again")
-		)
+		)*/
 	val sunnyTomorrow = If(sunnyToday, Flip(0.8), Flip(0.05))
 	val greetingTomorrow = If(sunnyTomorrow,
 		Select(0.5 -> "Hello, world!", 0.5 -> "Howdy, universe!"),
 		Select(0.1 -> "Hello, world!", 0.9 -> "Oh no, not again"))
 
 	def main(args: Array[String]) {
-	/*	println("We got out of the wrong side of the bed with probability " +
-			VariableElimination.probability(wrongSideOfBed, true))*/
+		println("We got out of the wrong side of the bed with probability " +
+			VariableElimination.probability(wrongSideOfBed, true))
 
 		val result = VariableElimination.probability(greetingTomorrow, "Hello, world!")
 	    println("If today's greeting is \"Hello, world!\", " +

1254617693b5d694835b19efd04e46d09cf70e5d
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-09-27 21:49:33 +0200

    [dice] predict a 6 and predict 3 doubles in a row

--- examples/src/main/scala/chap02/dice.scala
+++ examples/src/main/scala/chap02/dice.scala
@@ -9,11 +9,29 @@ object Dice {
 	val die2 = FromRange(1, 7)
 	val total = Apply(die1, die2, (a:Int, b:Int) => a + b)
 
-	def predict(n:Int):Double = {
+	def predictTotal(n:Int) = {
 		VariableElimination.probability(total, n)
+	}
+	def predict6() = {
+		total.addCondition((n:Int) => n > 8)
+		val result = VariableElimination.probability(die1, 6)
+		total.removeConditions()
+		result
+	}
+	def predictMonopolyGoToJail() = {
+		def doubles = {
+			die1 === die2
+		}
+		val jail = doubles && doubles && doubles
+		VariableElimination.probability(jail, true)
 	}
 
 	def main(args: Array[String]) {
-		println( predict(11) )
+		println( "Probability of rolling 11 is " + predictTotal(11) )
+
+		println( "Probability of rolling a 6 with the first die when the total is above 8 is " +
+			predict6() )
+
+		println( "Probability of rolling three doubles in a row is " + predictMonopolyGoToJail() )
 	}
 }
